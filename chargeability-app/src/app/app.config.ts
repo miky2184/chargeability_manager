b14ae15bdc87619f777031f27b84e5b6424086c8
Michele Micunco <michele.micunco@accenture.com>
2024-11-28 13:22:42 +0100

    modifica tabelle graph

--- chargeability-app/src/app/app.config.ts
+++ chargeability-app/src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(), // Aggiungi il supporto per le richieste HTTP
-    provideRouter(routes), // Configura il router con le rotte definite
+    provideRouter(routes), provideAnimationsAsync(), // Configura il router con le rotte definite
   ],
 };
--- chargeability-app/src/app/features/time-report/time-report.component.ts
+++ chargeability-app/src/app/features/time-report/time-report.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimeReportsService } from './time-reports.service';
+import { jsPDF } from 'jspdf';
+import autoTable from 'jspdf-autotable';
+import { MatButtonModule } from '@angular/material/button';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
 
 @Component({
   selector: 'app-time-report',
   standalone: true,
-  imports: [CommonModule], // Importa CommonModule per direttive come *ngIf e *ngFor
+  imports: [CommonModule, MatButtonModule, NgxDatatableModule, MatTableModule, MatPaginatorModule, MatSortModule], // Importa CommonModule per direttive come *ngIf e *ngFor
   templateUrl: './time-report.component.html',
   styleUrls: ['./time-report.component.css'],
 })
@@ -27,5 +35,30 @@ export class TimeReportComponent implements OnInit {
         this.isLoading = false;
       }
     );
+  }
+
+  exportToPDF(): void {
+    const doc = new jsPDF();
+
+    // Aggiungi titolo
+    doc.setFontSize(16);
+    doc.text('Time Reports', 10, 10);
+
+    // Genera la tabella
+    autoTable(doc, {
+      startY: 20,
+      head: [['EID', 'Anno', 'Mese', 'Progetto', 'Ore (TR1)', 'Ore (TR2)']],
+      body: this.timeReports.map((report) => [
+        report.eid,
+        report.yy_cal,
+        report.mm_cal,
+        report.project_name,
+        report.work_hh_tr1,
+        report.work_hh_tr2,
+      ]),
+    });
+
+    // Salva il PDF
+    doc.save('time-reports.pdf');
   }
 }
